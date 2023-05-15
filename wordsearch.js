845e03aa2b0907ab2ff036702b0206dce444aa76
arorac2 <88734236+arorac2@users.noreply.github.com>
2023-05-15 11:26:53 -0600

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0){
+      return false;
+  }
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let str = '';
+    for (let j = 0; j < letters.length; j++) {
+      str += letters[j][i];
+    }
+    verticalJoin.push(str);
+  }
+
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+
+
+module.exports = wordSearch;
